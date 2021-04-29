5552547d7e2318c13c35e5530e75f0ca16c63813
rysa <pyheejin@github.com>
2021-04-29 14:18:09 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6ea6b3a0bd937fb48479.js"
+    "url": "webpack-runtime-ebbe55d9e0ee6af53779.js"
   },
   {
     "url": "styles.96f49c0495c493b87405.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1d6ee24f1592df2334c47b7b5f2d955a"
+    "revision": "86e48345d3287dce61dcab954c5e34d1"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
