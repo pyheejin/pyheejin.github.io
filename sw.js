974819b228cde490d20089187fc1e8340eb56a6e
pyheejin <pyheejin@gmail.com>
2024-01-24 15:40:43 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.dfda73edc23d13796c54.css"
   },
   {
-    "url": "app-0e3d300911b5e7f6589a.js"
+    "url": "app-cb1f2d01cca20869153e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ec024d7472e9883a6bf0a11b011be7cb"
+    "revision": "da9f435d271c3cde91b654a371563132"
   },
   {
     "url": "manifest.webmanifest",
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-0e3d300911b5e7f6589a.js`))) {
+  if (!resources || !(await caches.match(`/app-cb1f2d01cca20869153e.js`))) {
     return await fetch(event.request)
   }
 
