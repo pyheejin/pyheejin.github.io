1bfdc44ba1a0691f0630b71817092eacf2410f81
pyheejin <pyheejin@gmail.com>
2024-01-24 17:59:16 +0900

    ** 20240124 [branch:develop]
    - 사이드바 태그 목록 추가

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { useSiteMetadata } from "@/hooks";
+import { useSiteMetadata, useTagsList } from "@/hooks";
 
 import { Author } from "./Author";
 import { Contacts } from "./Contacts";
 import { Copyright } from "./Copyright";
 import { Menu } from "./Menu";
+import { Tags } from '../Post/Tags';
 
 import * as styles from "./Sidebar.module.scss";
 
@@ -15,6 +16,7 @@ type Props = {
 
 const Sidebar = ({ isIndex }: Props) => {
   const { author, copyright, menu } = useSiteMetadata();
+  const tags = useTagsList();
 
   return (
     <div className={styles.sidebar}>
@@ -23,6 +25,10 @@ const Sidebar = ({ isIndex }: Props) => {
         <Menu menu={menu} />
         <Contacts contacts={author.contacts} />
         <Copyright copyright={copyright} />
+        <Tags
+          tags={tags.map((tag) => tag.fieldValue)}
+          tagSlugs={tags.map((tag) => `/tag/${tag.fieldValue}`)}
+        />
       </div>
     </div>
   );
