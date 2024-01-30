4fdbb1817fd47b49ad7e47974a2671361b170e69
pyheejin <pyheejin@gmail.com>
2024-01-30 16:02:54 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4d3ddb83d3dbd31630f9.js"
+    "url": "webpack-runtime-d69899a5dba2beac8978.js"
   },
   {
     "url": "framework-745d5d28c0250a1a333b.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.8369b489e007908a5c7b.css"
   },
   {
-    "url": "app-262a2323f3049a68f49d.js"
+    "url": "app-1f59dd9c9d6a15ea50ee.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "07f8fd12e6faedbd63546ce61783f8bd"
+    "revision": "fb13b2e184554a5823f21341b61feef3"
   },
   {
     "url": "manifest.webmanifest",
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-262a2323f3049a68f49d.js`))) {
+  if (!resources || !(await caches.match(`/app-1f59dd9c9d6a15ea50ee.js`))) {
     return await fetch(event.request)
   }
 
